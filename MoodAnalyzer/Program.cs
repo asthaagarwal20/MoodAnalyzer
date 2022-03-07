55cca8e12984a10457c485d50c0360bba4b0d519
unknown <asthaagarwal203@gmail.com>
2022-03-07 19:35:31 +0530

    [Astha] Add: Mood Analyser to analyse the mood is sad or happy

--- MoodAnalyzer/Program.cs
+++ MoodAnalyzer/Program.cs
@@ -8,31 +8,6 @@ namespace MoodAnalyzer
 {
     public class Program
     {
-
-        public string Test(string mood)
-        {
-            bool found = false;
-            string temp = null;
-            mood = mood + " ";
-            for(int i=0;i<mood.Length;i++)
-            {
-                if (mood[i] != ' ')
-                    temp=temp + mood[i];
-                else
-                {
-                    if (temp == "sad")
-                    {
-                        found = true;
-                        break;
-                    }
-                    temp = "";
-                }
-            }
-            if (found)
-                return "sad";
-            else
-                return "happy";
-        }
         static void Main(string[] args)
         {
         }
