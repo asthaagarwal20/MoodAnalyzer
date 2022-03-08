6633fa4db08adde00d312ab239f4661edc79dcea
unknown <asthaagarwal203@gmail.com>
2022-03-08 08:25:36 +0530

    [Astha] Add: MoodAnalyzerUC3 Inform user of invalid mood

--- MSTesting/MoodAnalyzerTestCases.cs
+++ MSTesting/MoodAnalyzerTestCases.cs
@@ -6,12 +6,22 @@ namespace MSTesting
     public class MoodAnalyzerTestCases
     {
         [TestMethod]
-        public void GivenNullMood_SoReturnHappy()
+        public void GivenEmptyMood_SoReturnEmptyMessagePassed()
+        {
+            MoodAnalyser moodAnalyzer = new MoodAnalyser();
+            string mood = string.Empty;
+            string expected = "Empty message passed";
+            string actual=moodAnalyzer.AnalyzeMood(mood);
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void GivenNullMood_SoReturnNullMessagePassed()
         {
             MoodAnalyser moodAnalyzer = new MoodAnalyser();
             string mood = null;
-            string expected = "happy";
-            string actual=moodAnalyzer.AnalyzeMood(mood);
+            string expected = "Null message passed";
+            string actual = moodAnalyzer.AnalyzeMood(mood);
             Assert.AreEqual(expected, actual);
 
         }
